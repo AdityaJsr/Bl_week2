2a1a0fde3aed1295ddd1738d1b3b0c584fa3f053
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 15:26:01 +0530

    concatinateList.py: completed

--- week 2/basicPython/concatinateList.py
+++ week 2/basicPython/concatinateList.py
@@ -1,5 +1,10 @@
-# Write a Python program to concatenate all elements in a list into a string and return it.
+"""
+Title - Write a Python program to concatenate all elements in a list into a string and return it.
+Author name - Aditya Kumar
+Ceation time - ‎‎02 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎02 ‎March ‎2021‎
 
+"""
 
 
 
@@ -19,9 +24,3 @@ def converStr():
 if __name__ == '__main__':
     converStr()
 
-# listItems = ['Hello', 'Rahul', "what's" , 'up'] 
-# output = ''
-# for items in listItems:
-#     output += str(items)+ ' '
-
-# print(output)
