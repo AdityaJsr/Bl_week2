815f10f911316937e00ca6c334d0693a327b84e8
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 12:47:57 +0530

    colorFirstLast.py: added color set problem

---
+++ week 2/basicPython/colorFirstLast.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program to display the first and last colors from the following list
+"""
+# List of the color.
+color_list = ["Red","Green","White" ,"Black"]
+
+# Printing the first and last memeber of the list/.
+print(color_list[0],color_list[-1])
