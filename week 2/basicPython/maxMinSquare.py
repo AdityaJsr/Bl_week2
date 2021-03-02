801447d54aaac20e87252f94bb332d740f318f88
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 23:55:44 +0530

    .\maxMinSquare.py

---
+++ week 2/basicPython/maxMinSquare.py
@@ -0,0 +1,20 @@
+"""
+Title - Write a Python function to find the maximum and minimum numbers from a sequence of
+numbers
+on operating system.
+Author name - Aditya Kumar
+Ceation time - ‎‎03 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎03 ‎March ‎2021‎
+
+"""
+def max_min(data):
+  l = data[0]
+  s = data[0]
+  for num in data:
+    if num> l:
+      l = num
+    elif num< s:
+        s = num
+  return l, s
+
+print(max_min([0, 10, 15, 40, -5, 42, 17, 28, 75]))
