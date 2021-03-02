b689c2ec3aa35ccc6bf291ee0808e8f8d068553d
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 15:28:04 +0530

    externalCommand.py: Completed

--- week 2/basicPython/externalCommand.py
+++ week 2/basicPython/externalCommand.py
@@ -1,2 +1,10 @@
+"""
+Title - Write a Python display external command.
+Author name - Aditya Kumar
+Ceation time - ‎‎02 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎02 ‎March ‎2021‎
+
+"""
+
 from subprocess import call
 call(["ls", "-l"])
