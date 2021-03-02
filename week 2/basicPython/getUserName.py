e50d640e8027766861ecf6fdbb1cfc3f63beb10c
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 15:29:40 +0530

    getUserName.py: completed

--- week 2/basicPython/getUserName.py
+++ week 2/basicPython/getUserName.py
@@ -1,7 +1,11 @@
-'''
-Write a Python program to get the current username
+"""
+Title - Write a Python program to get the current username.
+Author name - Aditya Kumar
+Ceation time - ‎‎02 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎02 ‎March ‎2021‎
 
-'''
+"""
+
 
 
 import getpass
