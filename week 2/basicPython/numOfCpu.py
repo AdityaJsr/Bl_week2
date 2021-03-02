6ebe935754ee56e1f60c11bf014a8b7bf84ec94c
AdityaJsr <adityajsr1996@gmail.com>
2021-03-02 15:32:18 +0530

    numOfCpu.py: completed

--- week 2/basicPython/numOfCpu.py
+++ week 2/basicPython/numOfCpu.py
@@ -1,7 +1,10 @@
-'''
-Write a Python program to find out the number of CPUs using
+"""
+Title - Write a Python program to find out the number of CPUs using.
+Author name - Aditya Kumar
+Ceation time - ‎‎02 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎02 ‎March ‎2021‎
 
-''' 
+"""
 
 import multiprocessing
 
