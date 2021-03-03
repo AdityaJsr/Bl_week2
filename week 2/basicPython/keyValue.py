0366c6d5babbb71ae6da9dbbf691599a912527b9
AdityaJsr <adityajsr1996@gmail.com>
2021-03-03 17:09:11 +0530

    keyValue.py: added

---
+++ week 2/basicPython/keyValue.py
@@ -0,0 +1,12 @@
+"""
+Title - Write a Python program to extract single key-value pair of a dictionary in variables.
+Author name - Aditya Kumar
+Ceation time - ‎‎03 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎03 ‎March ‎2021‎
+
+"""
+
+d = {'Red': 'Green'}
+(c1, c2), = d.items()
+print(c1)
+print(c2)
