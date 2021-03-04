3c7651e7c606f3066a5d90fbca0eebb2152d5639
AdityaJsr <adityajsr1996@gmail.com>
2021-03-04 12:48:09 +0530

    delVal.py: static code implemented

---
+++ week 2/dictP/delVal.py
@@ -0,0 +1,14 @@
+"""
+Title - Write a Python program to remove a key from a dictionary.
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+
+
+myDict = {'a':1,'b':2,'c':3,'d':4}
+print(myDict)
+if 'a' in myDict: 
+    del myDict['a']
+print(myDict)
