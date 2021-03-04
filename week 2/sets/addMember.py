18b6b89811fa3076730c0e1d4f7996b2f28e06e1
AdityaJsr <adityajsr1996@gmail.com>
2021-03-05 00:12:23 +0530

    addMember.py: added

---
+++ week 2/sets/addMember.py
@@ -0,0 +1,17 @@
+"""
+Title -  Write a Python program to add member(s) in a set.
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+
+color_set = set()
+color_set.add("Red")
+print(color_set)
+#Update multiple items
+color_set.update(["Blue", "Green"])
+print(color_set)
+a = input("Enter the color you want to ad to the set : ")
+color_set.add(a)
+print(color_set)
