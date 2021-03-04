6f98536aa7fcfd3d23700d564ae8a6287a7d961f
AdityaJsr <adityajsr1996@gmail.com>
2021-03-04 23:22:54 +0530

    countVal.py: added

---
+++ week 2/dictP/countVal.py
@@ -0,0 +1,15 @@
+"""
+Title -  Write a Python program to count the values associated with key in a dictionary.
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+
+student = [{'id': 1, 'success': True, 'name': 'Lary'},
+ {'id': 2, 'success': False, 'name': 'Rabi'},
+ {'id': 3, 'success': True, 'name': 'Alex'}]
+
+print(sum(d['id'] for d in student))
+
+print(sum(d['success'] for d in student))
