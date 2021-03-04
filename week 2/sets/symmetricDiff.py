923e5a99a0cbb0e353f48a41a8da22ed21c6436e
AdityaJsr <adityajsr1996@gmail.com>
2021-03-05 00:53:45 +0530

    symmetricDiff.py: added

---
+++ week 2/sets/symmetricDiff.py
@@ -0,0 +1,13 @@
+"""
+Title - Write a Python program to create a symmetric difference.
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+setx = set(["apple", "mango", "ranch"])
+sety = set(["mango", "orange"])
+#Symmetric difference
+setc = setx ^ sety
+print(setc)
+
