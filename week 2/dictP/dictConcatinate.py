0d0e0c31efc8819e114bcde8ca547ca1f2b9dd74
AdityaJsr <adityajsr1996@gmail.com>
2021-03-04 12:16:35 +0530

    dictConcatinate.py: added

---
+++ week 2/dictP/dictConcatinate.py
@@ -0,0 +1,29 @@
+"""
+Title -  Write a Python script to concatenate following dictionaries to create a new one.
+        Sample Dictionary :
+        dic1={1:10, 2:20}
+        dic2={3:30, 4:40}
+        dic3={5:50,6:60}
+        Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60} 
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+d4 = {}
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+dic1.update(dic2)
+dic1.update(dic3)
+
+
+# d4.update(dic1)
+# d4.update(dic2)
+# d4.update(dic3)
+
+print (dic1)
+
+
