0692515d3c7637eb2e1fae4ba16861d4a897e220
AdityaJsr <adityajsr1996@gmail.com>
2021-03-05 00:43:13 +0530

    intersection.py: added

---
+++ week 2/sets/intersection.py
@@ -0,0 +1,13 @@
+"""
+Title - Write a Python program to create an intersection of sets.
+Author name - Aditya Kumar
+Ceation time - ‎‎04 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎04 ‎March ‎2021‎
+
+"""
+
+#Intersection
+setx = set(["physics", "chemistry","biology","history"])
+sety = set(["math", "chemistry","hindi","english","history"])
+setz = setx & sety
+print(setz)
