aa1ff765a1138fc4a1984689fb54e568ffd18509
AdityaJsr <adityajsr1996@gmail.com>
2021-03-04 11:12:17 +0530

    .\sort.py: First commit

---
+++ week 2/dictP/sort.py
@@ -0,0 +1,10 @@
+"""
+Title - Write a Python script to sort (ascending and descending) a dictionary by value , 
+Access individual element through indexes.
+Author name - Aditya Kumar
+Ceation time - ‎‎03 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎03 ‎March ‎2021‎
+
+"""
+
+
