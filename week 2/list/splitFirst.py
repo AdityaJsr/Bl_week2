d744e4e502b5371fbec3be9c96ead236da5d9a0c
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 02:13:40 +0530

    splitFirst.py: added

---
+++ week 2/list/splitFirst.py
@@ -0,0 +1,18 @@
+"""
+Title - Write a Python program to split a list based on first character of word.
+Author name - Aditya Kumar
+Ceation time - ‎‎05 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎05 ‎March ‎2021‎
+
+""" 
+
+from itertools import groupby
+from operator import itemgetter
+
+word_list = ['be','have','do','say','get','make','go','know','take','see','come','think',
+     'look','want','give','use','find','tell','ask','work','seem','feel','leave','call']
+
+for letter, words in groupby(sorted(word_list), key=itemgetter(0)):
+    print(letter)
+    for word in words:
+        print(word)
