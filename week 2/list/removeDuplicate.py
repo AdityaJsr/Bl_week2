660e848496498d7484ae118d572734c58084c2a2
AdityaJsr <adityajsr1996@gmail.com>
2021-03-05 12:26:43 +0530

    removeDuplicate.py: adde

---
+++ week 2/list/removeDuplicate.py
@@ -0,0 +1,20 @@
+"""
+Title -  Write a Python program to remove duplicates from a list.
+Author name - Aditya Kumar
+Ceation time - ‎‎05 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎05 ‎March ‎2021‎
+
+""" 
+
+a = [10,20,30,20,10,50,60,40,80,50,40]
+
+dup_items = set()
+uniq_items = []
+for x in a:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
+
+# print(dup_items)
+print(uniq_items)
+
