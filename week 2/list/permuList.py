a63a9d7db077db95f16ea9420e9359939f1a65af
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 01:27:41 +0530

    permuList.py: added

---
+++ week 2/list/permuList.py
@@ -0,0 +1,11 @@
+"""
+Title - Write a Python program to generate all permutations of a list in Python.
+Author name - Aditya Kumar
+Ceation time - ‎‎05 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎05 ‎March ‎2021‎
+
+""" 
+import itertools
+
+list1 = [1, 2 , 3, 4]
+print(list(itertools.permutations(list1)))
