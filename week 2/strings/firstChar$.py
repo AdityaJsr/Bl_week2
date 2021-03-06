400ae1f25097c6744bc8a3b7b1d604989b86b1f2
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 16:15:25 +0530

    updated: dynamic

--- week 2/strings/firstChar$.py
+++ week 2/strings/firstChar$.py
@@ -20,18 +20,3 @@ if __name__ == "__main__":
     changeChar() 
 
 
-
-
-
-
-
-# def change_char(str1):
-#   char = str1[0]
-#   str1 = str1.replace(char, '$')
-#   str1 = char + str1[1:]
-
-#   return str1
-
-# print(change_char('sassy'))
-
-
