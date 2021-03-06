7531c3666a4794a20b2a0a062d7e57eb179c60ed
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 12:37:04 +0530

    .\convList2Tuple.py: added

---
+++ week 2/tuple/convList2Tuple.py
@@ -0,0 +1,14 @@
+"""
+Title - Write a Python program to convert a list to a tuple.
+Author name - Aditya Kumar
+Ceation time - ‎‎06 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎06 ‎March ‎2021‎
+
+""" 
+
+#Convert list to tuple
+li = [5, 10, 7, 4, 15, 3]
+print(li)
+#use the tuple() function built-in Python, passing as parameter the list
+tu = tuple(li)
+print(tu)
