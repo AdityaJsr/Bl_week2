288b410742baf283ca9498c7a22b06065ea878c0
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 13:17:13 +0530

    .\lenOfString.py: added

--- week 2/strings/lenOfString.py
+++ week 2/strings/lenOfString.py
@@ -5,5 +5,5 @@ Ceation time - ‎‎06 ‎March ‎2021 ‏‎
 Modified time - ‎‎‎06 ‎March ‎2021‎
 
 """ 
-
-
+userInput = input("Enter the string you want to know the length of : ")
+print(userInput, len(userInput))
