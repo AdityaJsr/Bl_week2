11531a19d923f3e69f3a6cfa36d09c80d13deb16
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 12:35:49 +0530

    .\sliceTuple.py: added

---
+++ week 2/tuple/sliceTuple.py
@@ -0,0 +1,14 @@
+"""
+Title - Write a Python program to slice a tuple.
+Author name - Aditya Kumar
+Ceation time - ‎‎06 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎06 ‎March ‎2021‎
+
+""" 
+
+tu = tuple(input("Enter the values of the tuple : "))
+print(tu)
+s = int(input("Enter the starting index : "))
+e = int(input("Enter the starting index : "))
+_ = tu[s:e]
+print(_)
