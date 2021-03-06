54df39ed1febcad4d9c72d51a7b452dc3e711581
AdityaJsr <adityajsr1996@gmail.com>
2021-03-06 16:40:10 +0530

    toUpperLower.py: added

---
+++ week 2/strings/toUpperLower.py
@@ -0,0 +1,12 @@
+"""
+Title - Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+Author name - Aditya Kumar
+Ceation time - ‎‎06 ‎March ‎2021 ‏‎
+Modified time - ‎‎‎06 ‎March ‎2021‎
+
+""" 
+userInput = input("Enter the string you want to know the length of : ")
+print("The orignal string : ",userInput)
+print("The toUpper converted string : ",userInput.upper())
+print("The toLower converted string : ",userInput.lower())
+
