5739b2a670c46fcd4e2fece668c309fc045ade24
AdityaJsr <adityajsr1996@gmail.com>
2021-03-08 13:29:11 +0530

    Updated with else statement

--- week 2/strings/addIng.py
+++ week 2/strings/addIng.py
@@ -10,6 +10,8 @@ Ceation time - ‎‎06 ‎March ‎2021 ‏‎
 Modified time - ‎‎‎06 ‎March ‎2021‎
 
 """ 
+# Enter the else statement
+
 def add_string():
     str1 = input("Enter the input string : ")
     len1 = len(str1)
@@ -18,6 +20,9 @@ def add_string():
             str1 += 'ly'
         else:
             str1 += 'ing'
+    else:
+        print("The minimum characters are not present : ",len(str1))
+        add_string()
     
     print(str1)
 
